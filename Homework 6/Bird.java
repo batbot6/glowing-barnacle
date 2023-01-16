1b52e1d944433eb8640ed83e1e4609e3d1e1cb53
batbot6 <116071046+batbot6@users.noreply.github.com>
2023-01-16 18:43:41 +0200

    Add files via upload

---
+++ Homework 6/Bird.java
@@ -0,0 +1,20 @@
+package Homework6;
+
+public class Bird extends Animal {
+
+    public void makesomenoise() {
+        System.out.println("Tzvurrr!");}
+    public void play() {
+        System.out.println("Piuuu! playful");
+    }
+        
+        public void sync() {
+            System.out.println("Bird is singing");
+        }
+        
+        public void sync(Bird b1, Bird b2) {
+            System.out.println("Birds are singing more");
+        }
+
+    }
+
